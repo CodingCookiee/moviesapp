6b0a2434a0daaaea5bb6116b8ae09a578a4c6a5d
CodingCookiee <rzqutubawan@gmail.com>
2024-11-29 16:59:54 +0500

    Update

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://moviesapp-api-theta.vercel.app/api/",
+  baseURL: "http://localhost:8000/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
