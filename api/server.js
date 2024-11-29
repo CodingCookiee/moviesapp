4d7fb93a22c58953d87b6d8ecf6b77a545a979a9
CodingCookiee <rzqutubawan@gmail.com>
2024-11-29 17:15:21 +0500

    Update

--- api/server.js
+++ api/server.js
@@ -24,6 +24,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["set-cookie"],
   }),
 );
 
--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: "https://moviesapp-api-theta.vercel.app/api",
-  timeout: 15000,
+  timeout: 30000, // Increased timeout
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": true
   }
 });
 
